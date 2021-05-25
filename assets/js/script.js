2de0119674f8eb6587f2c7e27573dc79059d9920
unknown <jean-bateee@hotmail.fr>
2021-05-25 09:41:31 +0200

    add scrolltop

--- assets/js/script.js
+++ assets/js/script.js
@@ -268,6 +268,32 @@ function clickNavButton(params) {
 
 
 
+function scrollToTop() {
+  const btn = document.getElementById('scrollTop')
+
+  window.onscroll = function() {scrollFunction()};
+
+  function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      btn.style.display = "block";
+    } else {
+      btn.style.display = "none";
+    }
+  }
+  btn.addEventListener('click', () => {
+    topFunction()
+  })
+
+  function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  }
+}
+scrollToTop()
+
+
+
+
 
 
 
